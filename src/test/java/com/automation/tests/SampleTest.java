fd185c060c5412afde98b372748f58f264409c92
Amr Rashad <amr.rashad1999@gmail.com>
2025-03-16 21:49:55 +0000

    Added automation framework for Amazon test scenario

---
+++ src/test/java/com/automation/tests/SampleTest.java
@@ -0,0 +1,11 @@
+package com.automation.tests;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SampleTest {
+    @Test
+    public void testExample() {
+        assertTrue(true);
+    }
+}
